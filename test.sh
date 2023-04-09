05822bd43aff9721b82ac1e0bc267211b7735056
dmytro108 <dmitry.fedunets@gmail.com>
2023-04-10 00:24:34 +0100

    fix eval

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ cat /tmp/first_bash_output
 
 
 echo "Directory  $1 contains"
-files = eval("ls -Al $1 | grep ^d | wc -l")
-dires = eval("ls -Al $1 | grep ^\[-\|l\] | wc -l")
+files = eval "ls -Al $1 | grep ^d | wc -l"
+dires = eval "ls -Al $1 | grep ^\[-\|l\] | wc -l"
 echo Dirictories: $dires Files: $files
 
