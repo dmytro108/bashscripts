b86b73f1a0f57feec303adf71828309c23288977
dmytro108 <dmitry.fedunets@gmail.com>
2023-04-09 23:44:20 +0100

    file counter

--- test.sh
+++ test.sh
@@ -3,3 +3,10 @@
 echo Hello World!
 date > /tmp/first_bash_output
 pwd >> /tmp/first_bash_output
+cat /tmp/first_bash_output
+
+echo Dirictories:
+ls -Al /var | grep ^d | wc -l
+
+echo Files:
+ls -Al /var | grep ^\[-\|l\] | wc -l
