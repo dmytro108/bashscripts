7be1c844b1d11ea5c36ff3843e8d6e2c545d0cc5
dmytro108 <dmitry.fedunets@gmail.com>
2023-04-10 00:56:22 +0100

    check for path

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ date > /tmp/first_bash_output
 pwd >> /tmp/first_bash_output
 cat /tmp/first_bash_output
 
-if [-n $1]; then
+if [ -e "$1" ]; then
     echo Directory  $1 contains
     files=$(ls -Al $1 | grep ^d | wc -l)
     dires=$(ls -Al $1 | grep ^\[-\|l\] | wc -l)
