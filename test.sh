3bffcabca043881f8874c64c5c0950232f6b9d08
dmytro108 <dmitry.fedunets@gmail.com>
2023-04-10 00:52:09 +0100

    Check for params

--- test.sh
+++ test.sh
@@ -5,8 +5,11 @@ date > /tmp/first_bash_output
 pwd >> /tmp/first_bash_output
 cat /tmp/first_bash_output
 
-echo Directory  $1 contains
-files=$(ls -Al /var | grep ^d | wc -l)
-dires=$(ls -Al /var | grep ^\[-\|l\] | wc -l)
-echo Dirictories: $dires Files: $files
-
+if [-n $1]; then
+    echo Directory  $1 contains
+    files=$(ls -Al $1 | grep ^d | wc -l)
+    dires=$(ls -Al $1 | grep ^\[-\|l\] | wc -l)
+    echo Dirictories: $dires Files: $files
+else
+    echo No path was provided
+fi
