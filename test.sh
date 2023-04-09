f47c3c9c115a37da6b96f2e653a7237a8d0c73d1
dmytro108 <dmitry.fedunets@gmail.com>
2023-04-10 00:45:35 +0100

    Fix variables expression

--- test.sh
+++ test.sh
@@ -5,9 +5,8 @@ date > /tmp/first_bash_output
 pwd >> /tmp/first_bash_output
 cat /tmp/first_bash_output
 
-
-echo "Directory  $1 contains"
-files = $(ls -Al $1 | grep ^d | wc -l)
-dires = $(ls -Al $1 | grep ^\[-\|l\] | wc -l)
+echo Directory  $1 contains
+files=$(ls -Al /var | grep ^d | wc -l)
+dires=$(ls -Al /var | grep ^\[-\|l\] | wc -l)
 echo Dirictories: $dires Files: $files
 
